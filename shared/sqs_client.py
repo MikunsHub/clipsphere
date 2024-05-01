f1129a9f9c10f93ea132222c3acccd1187ddae72
MikunsHub <ogunjuay@gmail.com>
2024-05-01 12:16:06 +0100

    migrated all interfaces to the shared module

--- shared/sqs_client.py
+++ shared/sqs_client.py
@@ -1,14 +1,21 @@
+from typing import NamedTuple, Optional
+
 import boto3
+from botocore.exceptions import ClientError
 
-LOCAL = 1
-LOCALSTACK_ENDPOINT_URL='http://localhost:4566'
 
-class S3Interface:
-	def __init__(self):
-		self.s3_client = self._create_s3_client()
+class Config(NamedTuple):
+	localstack_endpoint_url: Optional[str]
+	local: Optional[str]
 
-	def _create_s3_client(self):
-		if LOCAL:
-			return boto3.client('s3', endpoint_url=LOCALSTACK_ENDPOINT_URL)
-		return boto3.client('s3')
+class SQS_Interface:
+	def __init__(self,config:Config):
+		self.s3_client = self._create_sqs_client()
+		self.local = config.local
+		self.localstack_endpoint_url = config.localstack_endpoint_url
+
+	def _create_sqs_client(self):
+		if self.local:
+			return boto3.client('sqs', endpoint_url=self.localstack_endpoint_url)
+		return boto3.client('sqs')
 
